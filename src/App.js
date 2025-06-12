9111ad51eccca6867dec3da24dd1871a550cd57c
Yeon74 <freshms@daum.net>
2025-06-12 12:30:51 +0900

    quiz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+//1.퀴즈데이터 가져오기
+import quizData from "./data/quizData"
+import Categories from "./Components/Categories"
 
-function App() {
+const App = () => {
+  console.log(quizData)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Categories />
+      
     </div>
   );
-}
+};
 
 export default App;
